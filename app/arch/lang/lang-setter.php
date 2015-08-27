52ef7d1018261711555b8e9655ccc2451306093e
Sayef <bappasaif@gmail.com>
2015-08-28 02:16:44 +0600

    1. As I had to write code for every newly added xml for language settings in langSetter, I changed LangSetter.php for dynamically adding variable names. 2. Keep an extra xml 'varlist_path.xml' for the path list of the xmls used for different language

--- app/arch/lang/lang-setter.php
+++ app/arch/lang/lang-setter.php
@@ -4,72 +4,37 @@ Class LangSetter{
 
 	public function __construct(){
 
-
-			require_once(APP . 'libs/utility/xml-parser.php');
-			$x_path = new XMLparser();
+			//language from the session
 			$lang = $_SESSION['lang'];
 
-			//Header
 
-			$_SESSION['sitename'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/home/header.xml", "/header/sitename");
-			$_SESSION['sitetagline'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/home/header.xml", "/header/sitetagline");
+			//Read the path list to iterate through those paths to get the variable names 
+			$file = APP . 'arch/lang/varlist_path.xml'; 
+			$branch = simplexml_load_file($file);
 
+			// for every path
+			foreach ($branch->children() as $child){
 
-			//body
-			$_SESSION['signin_field'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/home/body.xml", "/body/signin_field");
-			$_SESSION['userid_field'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/home/body.xml", "/body/userid_field");
-			$_SESSION['password_field'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/home/body.xml", "/body/password_field");
-			$_SESSION['submit_value'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/home/body.xml", "/body/submit_value");
-			$_SESSION['new_account_field'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/home/body.xml", "/body/new_account_field");
+				$path = (string)$child;
 
+				//we find the file where variable names and their corresponding values are kept
+				$file = APP . 'arch/lang/' . $lang . $path;
+				$var_branch = simplexml_load_file($file);
 
-			//footer
-			$_SESSION['copyright'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/home/footer.xml", "/body/copyright");
+				//for every page
+				foreach ($var_branch->children() as $var_child) {
 
+					//we find the variable name by getname function 
+					$variable = (string)$var_child->getname();
 
-			//sign up failure message
-			$_SESSION['signinerror'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/popupmessage/popupmessage.xml", "/popupmessage/signinerror");
-			//sign up success message
-			$_SESSION['signupsuccess'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/popupmessage/popupmessage.xml", "/popupmessage/signupsuccess");
-			//sign up error message
-			$_SESSION['signuperror'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/popupmessage/popupmessage.xml", "/popupmessage/signuperror");
+					//now set your variable values according to their language
+					$_SESSION[$variable] = (string)$var_child;
 
-			//signip page 
-			$_SESSION['su_formname'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/signup/signup.xml", "/signup/su_formname");
-			$_SESSION['su_userid'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/signup/signup.xml", "/signup/su_userid");
-			$_SESSION['su_name'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/signup/signup.xml", "/signup/su_name");
-			$_SESSION['su_password'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/signup/signup.xml", "/signup/su_password");
-			$_SESSION['su_profession'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/signup/signup.xml", "/signup/su_profession");
-			$_SESSION['su_email'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/signup/signup.xml", "/signup/su_email");
-			$_SESSION['su_dateof_birth'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/signup/signup.xml", "/signup/su_dateof_birth");
-			$_SESSION['su_dateof_signup'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/signup/signup.xml", "/signup/su_dateof_signup");
-			$_SESSION['su_profile_picture'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/signup/signup.xml", "/signup/su_profile_picture");
-			$_SESSION['su_pass_ins'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/signup/signup.xml", "/signup/su_pass_ins");
-			$_SESSION['su_email_ins'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/signup/signup.xml", "/signup/su_email_ins");
-			$_SESSION['su_submit_button'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/signup/signup.xml", "/signup/su_submit_button");
-
-
-			//navbar values
-			$_SESSION['home'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/navbar/navbar.xml", "/navbar/home");
-			$_SESSION['category'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/navbar/navbar.xml", "/navbar/category");
-			$_SESSION['questions'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/navbar/navbar.xml", "/navbar/questions");
-			$_SESSION['search_question'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/navbar/navbar.xml", "/navbar/search_question");
-			$_SESSION['settings'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/navbar/navbar.xml", "/navbar/settings");
-			$_SESSION['tags'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/navbar/navbar.xml", "/navbar/tags");
-			$_SESSION['help'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/navbar/navbar.xml", "/navbar/help");
-			$_SESSION['submit'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/navbar/navbar.xml", "/navbar/submit");
-
-		
-			//ask questions values
-
-			$_SESSION['ask_question'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/dashboard/dashboard.xml", "/dashboard/ask_question");
-			$_SESSION['add_tags'] = $x_path->my_xpath(APP . "arch/lang/".$lang."/dashboard/dashboard.xml", "/dashboard/add_tags");
-
-
-
+				}
 
 		}
 	}
+}
 
 
 ?>
